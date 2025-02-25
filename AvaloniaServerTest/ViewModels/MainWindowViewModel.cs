633b350002dada0177eb4e1692af814f39e5d1e7
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2025-02-24 20:00:18 -0500

    Avalonia new project.

---
+++ AvaloniaServerTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AvaloniaServerTest.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ AvaloniaServerTest/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AvaloniaServerTest.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
